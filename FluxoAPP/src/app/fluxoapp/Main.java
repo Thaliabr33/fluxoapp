c80d08c0e8ba181096d8da50ce9da2d09d1a8fff
= <=>
2025-08-01 11:55:40 -0300

    arrumado bug descrição

--- FluxoAPP/src/app/fluxoapp/Main.java
+++ FluxoAPP/src/app/fluxoapp/Main.java
@@ -29,14 +29,15 @@ operacao = leia.nextInt();
 switch (operacao) {
     case 1:
     System.out.println("Cadastrar movimentação!");
-    System.out.println("Digite a descrição: ");
-    novaMovimentacao.setDescricao(leia.next());
     System.out.println("Digite o valor: ");
     novaMovimentacao.setValor(leia.nextDouble());
     System.out.println("Digite a data (dd/mm/aaaa): ");
     novaMovimentacao.setData(leia.next());
     System.out.println("Digite a categoria: Despesa ou Receita: ");
     novaMovimentacao.setCategoria(leia.next());
+    System.out.println("Digite a descrição: ");
+    novaMovimentacao.setDescricao(leia.next());
+    
     novoControle.adicionarMovimentacao(novaMovimentacao);
     System.out.println("Movimentação cadastrada com sucesso!");
     break;
@@ -50,6 +51,8 @@ switch (operacao) {
     int index = leia.nextInt();
     novoControle.removerMovimentacao(index);
     break;
+    case 4:
+    System.out.println("Exibir saldo do usuário: R$" + novoUsuario.getSaldo());
     case 0:
     System.out.println("Saindo...");
     break;
@@ -66,6 +69,7 @@ public static void exibeMenu() {
     System.out.println("1 - Cadastrar Movimentação");
     System.out.println("2 - Listar Movimentações");
     System.out.println("3 - Remover Movimentação");
+    System.out.println("4 - Exibir Saldo do Usuário");
     System.out.println("0 - Sair");
     System.out.print("Escolha uma opção: ");
 }
