74f5f16ab80f48dd6de1196473443e90192af2d0
= <=>
2025-08-04 13:37:05 -0300

    Corrigido bug na exportação de dados e no arquivo run.bat que não estava aceitando terminais Windows

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 
 echo "Compilando..."
+
+# Cria a pasta bin se não existir
+mkdir -p bin
+
+# Compila todos os arquivos .java encontrados em src e subpastas
 javac -encoding UTF-8 -d bin $(find src -name "*.java")
 
 echo "Executando..."
